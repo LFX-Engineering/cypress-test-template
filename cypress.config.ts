92da11c2876e22467d90351aee6958f11a2b8fb0
Asitha de Silva <asithade@gmail.com>
2022-06-08 19:54:26 -0700

    Update Config and Add Image
    Signed-off-by: Asitha de Silva <asithade@gmail.com>

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,9 +4,6 @@ export default defineConfig({
   viewportWidth: 1920,
   viewportHeight: 1080,
   defaultCommandTimeout: 10000,
-  fixturesFolder: './fixtures',
-  screenshotsFolder: './screenshots',
-  videosFolder: './videos',
   e2e: {
     baseUrl: 'http://localhost:4200'
   }
